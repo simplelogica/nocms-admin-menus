31fae3ce2689a47af1081d6a9ac4a3bc99a137b5
David J. Brenes <david.brenes@simplelogica.net>
2014-03-27 17:24:35 +0100

    Updating menus
    This is a simple update, without any menu item

--- app/controllers/no_cms/admin/menus/menus_controller.rb
+++ app/controllers/no_cms/admin/menus/menus_controller.rb
@@ -5,7 +5,7 @@ module NoCms::Admin::Menus
 
     before_filter :load_menu_section
     before_filter :load_menus, only: [:index, :new, :edit]
-    before_filter :load_menu, only: [:edit]
+    before_filter :load_menu, only: [:edit, :update]
 
     def new
       @menu = NoCms::Menus::Menu.new
@@ -15,9 +15,20 @@ module NoCms::Admin::Menus
       @menu = NoCms::Menus::Menu.new menu_params
       if @menu.save
         @nocms_logger.info(I18n.t('.no_cms.admin.menus.menus.create.success'), true)
-        redirect_to action: :index
+        redirect_to edit_menu_path(@menu)
       else
         @nocms_logger.error(I18n.t('.no_cms.admin.menus.menus.create.error'))
+        load_menus
+        render :new
+      end
+    end
+
+    def update
+      if @menu.update_attributes menu_params
+        @nocms_logger.info(I18n.t('.no_cms.admin.menus.menus.update.success'), true)
+        redirect_to edit_menu_path(@menu)
+      else
+        @nocms_logger.error(I18n.t('.no_cms.admin.menus.menus.update.error'))
         load_menus
         render :new
       end
