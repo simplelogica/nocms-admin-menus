fb264a98653f6c0fd9fc8ab315be1f118a8dc8ab
David J. Brenes <david.brenes@simplelogica.net>
2014-03-27 16:19:42 +0100

    Migrations and schema for menus

---
+++ spec/dummy/db/schema.rb
@@ -0,0 +1,59 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20140327151929) do
+
+  create_table "no_cms_menus_menu_item_translations", force: true do |t|
+    t.integer  "no_cms_menus_menu_item_id"
+    t.string   "locale"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "external_url"
+  end
+
+  add_index "no_cms_menus_menu_item_translations", ["no_cms_menus_menu_item_id"], name: "no_cms_menu_item_on_translations"
+
+  create_table "no_cms_menus_menu_items", force: true do |t|
+    t.integer  "menu_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth"
+    t.integer  "menuable_id"
+    t.string   "menuable_type"
+    t.string   "menu_action"
+  end
+
+  add_index "no_cms_menus_menu_items", ["menu_id"], name: "index_no_cms_menus_menu_items_on_menu_id"
+  add_index "no_cms_menus_menu_items", ["menuable_id", "menuable_type"], name: "index_no_cms_menus_menu_items_on_menuable_id_and_menuable_type"
+
+  create_table "no_cms_menus_menu_translations", force: true do |t|
+    t.integer  "no_cms_menus_menu_id"
+    t.string   "locale"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "no_cms_menus_menu_translations", ["no_cms_menus_menu_id"], name: "index_no_cms_menus_menu_translations_on_no_cms_menus_menu_id"
+
+  create_table "no_cms_menus_menus", force: true do |t|
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
