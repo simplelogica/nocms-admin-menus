caa6747a1aec656c99d96afe843377128732e050
David J. Brenes <david.brenes@simplelogica.net>
2014-03-28 14:36:10 +0100

    Edit form for blocks. Right now we just can edit the name of each menu item

--- app/helpers/no_cms/admin/menus/menus_helper.rb
+++ app/helpers/no_cms/admin/menus/menus_helper.rb
@@ -7,6 +7,29 @@ module NoCms
           classes << 'current' if current_menu == menu
           classes
         end
+
+        def menu_item_form_classes menu_item
+          classes = []
+          classes << 'new' if menu_item.new_record?
+          classes
+        end
+
+        def menu_item_form_id menu_item
+          menu_item.new_record? ?
+          "new_content_block_#{menu_item.kind}" :
+          "content_block_#{menu_item.id}"
+        end
+
+        def render_menu_item_partial form
+          menu_item = form.object
+          case
+          when !menu_item.menu_kind[:object_class].nil?
+            options = menu_item.menu_kind[:object_class].all
+            render 'no_cms/admin/menus/menu_items/object_form', menu_item: menu_item, f: form, options: options
+          else
+            'zxvzxcvzxv'
+          end
+        end
       end
     end
   end
