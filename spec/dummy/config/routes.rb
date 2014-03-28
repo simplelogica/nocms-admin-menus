b6730364ec96befea4433af96c38abc6b43a0ae5
David J. Brenes <david.brenes@simplelogica.net>
2014-03-28 13:33:58 +0100

    Including testing controllers, models and views
    This we can play with models and controllers for configuring the Admin

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount NoCms::Admin::Engine => "/admin"
   mount NoCms::Admin::Menus::Engine => "/admin"
+
+  resources :products
+
+  resources :pages, only: [:index]
+  get '*path', to: 'pages#show'
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327151929) do
+ActiveRecord::Schema.define(version: 20140328111853) do
 
   create_table "no_cms_menus_menu_item_translations", force: true do |t|
     t.integer  "no_cms_menus_menu_item_id"
@@ -52,6 +52,18 @@ ActiveRecord::Schema.define(version: 20140327151929) do
 
   create_table "no_cms_menus_menus", force: true do |t|
     t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pages", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
