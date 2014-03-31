f8ef30eefa1b0f2cd407b17c32de8601c415e6ec
David J. Brenes <david.brenes@simplelogica.net>
2014-03-31 16:10:25 +0200

    Migration moved to nocms-menus

--- spec/dummy/db/migrate/20140328124531_add_kind_to_no_cms_menus_menu_item.no_cms_menus.rb
+++ spec/dummy/db/migrate/20140328124531_add_kind_to_no_cms_menus_menu_item.no_cms_menus.rb
@@ -1,3 +1,4 @@
+# This migration comes from no_cms_menus (originally 20140328123240)
 class AddKindToNoCmsMenusMenuItem < ActiveRecord::Migration
   def change
     add_column :no_cms_menus_menu_items, :kind, :string
