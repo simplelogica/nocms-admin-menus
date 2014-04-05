304a4e156af7578d484861657c5d820f25c4f2a0
David J. Brenes <david.brenes@simplelogica.net>
2014-04-05 16:42:27 +0200

    Added editing menu test

---
+++ spec/requests/editing_menus_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe NoCms::Admin::Menus do
+
+  context "when editing a menu", js: true do
+
+    let(:no_cms_menu) { create :no_cms_menus_menu }
+    let(:no_cms_menu_item) { create :no_cms_menus_menu_item, menu: no_cms_menu }
+
+    let(:menu_name) { Faker::Lorem.sentence }
+    let(:menu_name_item) { Faker::Lorem.sentence }
+
+    subject { page }
+
+    before do
+
+      no_cms_menu_item
+
+      visit no_cms_admin_menus.edit_menu_path(no_cms_menu)
+
+      within('#content_fields') do
+        fill_in I18n.t('activerecord.attributes.no_cms/menus/menu.name'), with: menu_name
+      end
+
+      within("#content_block_#{no_cms_menu_item.id}") do
+        fill_in I18n.t('activerecord.attributes.no_cms/menus/menu_item.name'), with: menu_name_item
+      end
+
+      click_button(I18n.t('no_cms.admin.menus.menus.toolbar_right.submit'))
+    end
+
+    it "should show the menu in the admin index" do
+      expect(current_path).to eq no_cms_admin_menus.edit_menu_path  NoCms::Menus::Menu.first
+      expect(subject).to have_selector 'li', text: menu_name
+    end
+
+    it "should show the menu item in the admin index" do
+      expect(current_path).to eq no_cms_admin_menus.edit_menu_path  NoCms::Menus::Menu.first
+      within("#content_block_#{no_cms_menu_item.id}") do
+        expect(subject).to have_selector '.title', text: menu_name_item
+      end
+    end
+
+  end
+end
