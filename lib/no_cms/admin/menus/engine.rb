f3469e6033a7908cd762d51b42011cca072599e4
David J. Brenes <davidjbrenes@gmail.com>
2014-11-13 09:53:41 +0100

    #1 Decorator for Menu Item to create the custom attribute for markingas a template

--- lib/no_cms/admin/menus/engine.rb
+++ lib/no_cms/admin/menus/engine.rb
@@ -3,6 +3,11 @@ module NoCms
     module Menus
       class Engine < ::Rails::Engine
         isolate_namespace NoCms::Admin::Menus
+        config.to_prepare do
+          Dir.glob(NoCms::Admin::Menus::Engine.root + "app/decorators/**/*_decorator*.rb").each do |c|
+            require_dependency(c)
+          end
+        end
       end
     end
   end
